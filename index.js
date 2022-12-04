88018292fb05f5a8ac229fe621bd9046ffb946cb
asaad227 <a.saad227@yahoo.com>
2022-12-04 22:49:29 +0000

    note added js file

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const txt = document.querySelector(".msg");
-function handleNetworkChange(){
-  navigator.onLine? txt.innerHTML = "You're connected 🔗": txt.innerHTML= "You're offline 😢" ;
-  navigator.onLine? txt.className = "msgreen": txt.className = "msred";  
- }
- 
-window.addEventListener('load', () => {
-  handleNetworkChange()
-    window.addEventListener('online', handleNetworkChange);
-    window.addEventListener('offline', handleNetworkChange);
-  });
+
+function handleNetworkChange() {
+  navigator.onLine
+    ? (txt.innerHTML = "You're connected 🔗")
+    : (txt.innerHTML = "You're offline 😢");
+    //passing the txt according to user selction
+  navigator.onLine ? (txt.className = "msgreen") : (txt.className = "msred");
+  //swaping the class according to user selection
+}
+
+window.addEventListener("load", () => {
+  handleNetworkChange();
+  //default function which will show last change
+  window.addEventListener("online", handleNetworkChange);
+  window.addEventListener("offline", handleNetworkChange);
+  //this will listen according to user selection
+});
